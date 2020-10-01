17e20b09a0ea436be412b10b4c9702927a1eccf6
Chueseng Vang <chewyvang@Chuesengs-MacBook-Pro.local>
2020-09-30 20:58:20 -0500

    Completed Activity-4

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
+import cors from 'cors';
 
 //Initialize express application
 const app = express();
@@ -10,6 +11,11 @@ connectDatabase();
 
 //configure Middleware
 app.use(express.json({ extended: false}));
+app.use(
+  cors({
+    origin: 'http://localhost:3000'
+  })
+);
 
 //API endpoint
 /**
@@ -50,4 +56,5 @@ app.use(express.json({ extended: false}));
 );
 
   // Connection listner
-  app.listen(3000, () => console.log ('Express server running on port 3000'));
+  const port = 5000;
+  app.listen(port, () => console.log ('Express server running on port ${port}'));
